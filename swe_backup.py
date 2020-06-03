34f397eb849e798d79023b58d7dfafc130e84357
Ramon Weeling <ramon@mikrozagt.com>
2020-06-03 16:08:57 +0200

    Update swe_backup.py

--- swe_backup.py
+++ swe_backup.py
@@ -41,29 +41,89 @@ SOFTWARE.
 
 
 import requests
+import getpass
 import json
+import sys
 import os
 try:
     requests.packages.urllib3.disable_warnings()
 except:
     pass
 
-# Enter all authentication info
-SMC_USER = ""
-SMC_PASSWORD = ""
-SMC_HOST = ""
-BACKUP_DIR = ""
+# Config Paramters
+CONFIG_FILE     = "config.json"
+CONFIG_DATA     = None
+
+# Object Prefix
+OBJECT_PREFIX = ""
+
+# A function to load CONFIG_DATA from file
+def loadConfig():
+
+    global CONFIG_DATA
+
+    sys.stdout.write("\n")
+    sys.stdout.write("Loading config data...")
+    sys.stdout.write("\n")
+
+    # If we have a stored config file, then use it, otherwise create an empty one
+    if os.path.isfile(CONFIG_FILE):
+
+        # Open the CONFIG_FILE and load it
+        with open(CONFIG_FILE, 'r') as config_file:
+            CONFIG_DATA = json.loads(config_file.read())
+
+        sys.stdout.write("Config loading complete.")
+        sys.stdout.write("\n")
+        sys.stdout.write("\n")
+
+    else:
+
+        sys.stdout.write("Config file not found, loading empty defaults...")
+        sys.stdout.write("\n")
+        sys.stdout.write("\n")
+
+        # Set the CONFIG_DATA defaults
+        CONFIG_DATA = {
+            "SMC_HOST": "",
+            "SMC_USER": "",
+            "SMC_PASSWORD": "",
+            "BACKUP_DIR": "",
+        }
+
+# A function to store CONFIG_DATA to file
+def saveConfig():
+
+    sys.stdout.write("Saving config data...")
+    sys.stdout.write("\n")
+
+    with open(CONFIG_FILE, 'w') as output_file:
+        json.dump(CONFIG_DATA, output_file, indent=4)
+
+# Load config data from file
+loadConfig()
+
+# If not hard coded, get the FMC IP, Username, and Password
+if CONFIG_DATA['SMC_HOST'] == '':
+    CONFIG_DATA['SMC_HOST'] = input("SMC IP Address: ")
+if CONFIG_DATA['SMC_USER'] == '':
+    CONFIG_DATA['SMC_USER'] = input("\nSMC Username: ")
+if CONFIG_DATA['SMC_PASSWORD'] == '':
+    CONFIG_DATA['SMC_PASSWORD'] = getpass.getpass("\nSMC Password: ")
+if CONFIG_DATA['BACKUP_DIR'] == '':
+    CONFIG_DATA['BACKUP_DIR'] = input("\nBACKUP Directory: ")
 
 # Get the list of current backupped files
-list_of_backups = os.listdir(BACKUP_DIR)
+list_of_backups = os.listdir(CONFIG_DATA['BACKUP_DIR'])
 
 # Set the URL for SMC login
-url = "https://" + SMC_HOST + "/token/v2/authenticate"
+url = "https://" + CONFIG_DATA['SMC_HOST'] +"/token/v2/authenticate"
+print(url)
 
 # Let's create the login request data
 login_request_data = {
-    "username": SMC_USER,
-    "password": SMC_PASSWORD
+    "username": CONFIG_DATA['SMC_USER'],
+    "password": CONFIG_DATA['SMC_PASSWORD']
 }
 
 # Initialize the Requests session
@@ -76,9 +136,14 @@ response = api_session.request("POST", url, verify=False, data=login_request_dat
 if(response.status_code == 200):
 
     # Set the url for getting the appliance list
-    url = "https://" + SMC_HOST + "/cm/inventory/appliances/"
+    url = "https://" + CONFIG_DATA['SMC_HOST'] +"/cm/inventory/appliances/"
 
     # Perform the query to get the appliance list
+
+    sys.stdout.write("\n")
+    sys.stdout.write("Getting the appliance list")
+    sys.stdout.write("\n")
+
     request_headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
     response = api_session.request("GET", url, verify=False,)
 
@@ -90,7 +155,7 @@ if(response.status_code == 200):
         list_ids.append(SW_appliance["id"])
 
     for id in list_ids:
-        url = "https://" + SMC_HOST + "/cm/support/appliance/" + id + "/config-backup-file-list"
+        url = "https://" + CONFIG_DATA['SMC_HOST'] +"/cm/support/appliance/" + id + "/config-backup-file-list"
         response = api_session.request("GET", url,  verify=False,)
 
         returned_data = json.loads(response.text)
@@ -110,18 +175,20 @@ if(response.status_code == 200):
             # Check if file is already backuped and download if its not
             for file in list_filenames:
                 if  not file in list_of_backups:
-                    print("" + file + " has not been downloaded yet, downloading it now")
-                    url = "https://" + SMC_HOST + "/cm/support/appliance/" + id + "/" + file + "/config-backup-file"
+                    sys.stdout.write("From appliance " + id + " we are missing " + file + " downloading")
+                    sys.stdout.write("\n")
+                    url = "https://" + CONFIG_DATA['SMC_HOST'] +"/cm/support/appliance/" + id + "/" + file + "/config-backup-file"
                     response = api_session.request("GET", url,  verify=False,)
                     myfilename = file
-                    mydirname = BACKUP_DIR
+                    mydirname = CONFIG_DATA['BACKUP_DIR']
                     myfullfilename = os.path.join(mydirname, myfilename)
                     zfile = open(myfullfilename, 'wb')
                     zfile.write(response.content)
                     zfile.close()
                 else:
-                    print("" + file + " has been downloaded already")
-    url = 'https://' + SMC_HOST + '/token'
+                    sys.stdout.write("" + file + " has been downloaded already")
+                    sys.stdout.write("\n")
+    url = "https://" + CONFIG_DATA['SMC_HOST'] +"/token"
     response = api_session.delete(url, timeout=30, verify=False)
 
 
